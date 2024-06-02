6a2d0cadd98f8146b04882b3611909a838c3bd66
designer-ab <per.useravi0@gmail.com>
2024-06-02 15:04:40 +0600

    added user collection and post method to collect user data

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 // middleware
 
-
 // mongoDB CURD
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zfuxqes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,13 +20,37 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // collection
+    const usersCollection = client.db("learnEaseDB").collection("users");
+    // collection
+
+    // database api start
+
+    // user related api
+
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const query = { email: userInfo.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // user related api
+
+    // database api end
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,22 +61,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // mongoDB CURD
-
-
-
-
-
-
-
 
 // root api
 app.get("/", (req, res) => {
   res.send("Learn Ease Running Successfully");
 });
 // root api
-
 
 app.listen(port, () => {
   console.log(`Learn Ease Running On Port ${port}`);
