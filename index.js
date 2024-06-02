746c71b05a45477bfae949ae0c47b19c2e47e833
designer-ab <per.useravi0@gmail.com>
2024-06-02 11:52:40 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 // middleware
 app.use(cors());
@@ -9,14 +13,51 @@ app.use(express.json());
 // middleware
 
 
+// mongoDB CURD
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zfuxqes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// mongoDB CURD
+
+
+
+
+
+
+
 
 // root api
-app.get('/', (req, res) => {
-    res.send("Learn Ease Running Successfully");
+app.get("/", (req, res) => {
+  res.send("Learn Ease Running Successfully");
 });
 // root api
 
-app.listen(port, () => {
-    console.log(`Learn Ease Running On Port ${port}`);
-})
 
+app.listen(port, () => {
+  console.log(`Learn Ease Running On Port ${port}`);
+});
